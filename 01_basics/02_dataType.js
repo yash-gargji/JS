2969f132e2bc6de9da4ff5927c20a09d93b931d4
Yash Garg <134330835+yash-gargji@users.noreply.github.com>
2024-05-23 21:50:06 +0530

    commit

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all Js code as newer version 
+
+// alert(3 + 3)  we are using node js not browser
+
+ // code readability must be high
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint 
+// string =>
+// boolean =>true/false
+// null => standalone value
+// undefined => varaoble declared but value not assigned 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
