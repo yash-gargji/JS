d6be17cb1df24e6c06505702d685300601ab8778
Yash Garg <134330835+yash-gargji@users.noreply.github.com>
2024-05-23 22:14:44 +0530

    commit

---
+++ 01_basics/03_converionOperations.js
@@ -0,0 +1,27 @@
+// let score = "33abc"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number (score)
+
+// console.log( (valueInNumber))
+
+// "33" => 33
+// "33abc" =>NaN
+// true => 1; false = 0;
+
+// let isLoggedIn =  null
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" =>flase 
+// "yash"  => true
+
+let num = ""
+let stringNumber = String(num)
+
+console.log(stringNumber)
