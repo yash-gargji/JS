770efef248e56320a4f1b0b094481868858f9765
Yash Garg <134330835+yash-gargji@users.noreply.github.com>
2024-05-25 00:18:00 +0530

    commit

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -19,5 +19,33 @@ let isLoggedIn = false;
 
 // object
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+// operations
+
+
+let value  = 3
+let negvalue = -value;
+// console.log(2**3) // power of a number format 
+
+let str1 = "hello"
+let str2 = " yash"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1 + "2a")
+// console.log(1 + 2 + "3")
+// console.log( "3" + 1 + 2 )
+
+console.log(+true) // 1 // not prefferable 
+
+let num1 ,num2 ,num3
+
+num1 = num2 = num3 = 2 + 2 // not prefeerable 
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter)
+
