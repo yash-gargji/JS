e30cdd4eb6681ac2e611c389dda9465cf5376dfc
Yash Garg <134330835+yash-gargji@users.noreply.github.com>
2024-05-25 01:47:21 +0530

    commit

---
+++ 01_basics/04_comaprisons.js
@@ -0,0 +1,21 @@
+console.log("2" > 1)
+console.log("02" > 1)
+
+// data type must be same 
+// we should must ensure that data types must be same 
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+// not a correct type of answer it will give 
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+// we will alwys avoid these type of conversions 
+
+// it will give always false
+
+// === strict check 
+
+console.log("2" === 2);
