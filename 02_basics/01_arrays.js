b300a541b23cce96a3275bb04faf0996208e6696
Yash Garg <134330835+yash-gargji@users.noreply.github.com>
2024-05-29 03:53:18 +0530

    commit

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+// array
+
+const  myArr = [0,1,2,3,4,5,true,"hitesh"]
+const myHeroes = ["SHaktiman", "nagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[0])
+
+// Array mwthods
+
+// myArr.push(6)
+// myArr.pop()
+
+// myArr.unshift(9)
+// // Takes O(N) time 
+
+// myArr.shift()
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3))
+
+// const newArr = myArr.join()
+
+// console.log(myArr)
+// console.log(newArr)
+// this has become string 
+
+console.log("A ",myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1)
+
+console.log("B ",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C ",myArr)
+// important in slice the section of the array just copied 
+// in the splice the section from the just cut out and in this range is also included 
+console.log(myn2)
