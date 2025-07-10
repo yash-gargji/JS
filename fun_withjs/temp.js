f3562f3bd91ed58990c651087dfc10c00812f14c
Yash Garg <yash.gargji2004@gmail.com>
2025-07-11 03:52:05 +0530

    commit

---
+++ fun_withjs/temp.js
@@ -0,0 +1,13 @@
+
+ 
+ function check() {
+
+    if(5){
+       var a = 10;
+    }
+    
+     console.log(a);
+ }
+ check();
+ 
+ 
---
+++ oops/properties_get_set.js
@@ -0,0 +1,14 @@
+function  User(email,password){
+     this._email = email;
+     this._password = password;
+
+     Object.defineProperty(this , 'email', {
+         get : function(){
+            return this._email;
+         },
+         set: function(value){
+            this._email = value
+         }
+       }
+     )
+}
